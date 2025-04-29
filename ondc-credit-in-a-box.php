1e07f7ea149614a121c87da38138c28a0f7aa028
hukeshbhagat <hukeshbhagat5@gmail.com>
2025-04-29 18:42:22 +0530

    Refactor code structure for improved readability and maintainability

--- ondc-credit-in-a-box.php
+++ ondc-credit-in-a-box.php
@@ -27,12 +27,13 @@
 <section class="relative overflow-hidden py-10 pt-20 md:pt-24 md:py-10 bg-white">
     <!-- Top-left Circular Gradient (Pinkish) - Exactly 50% visible -->
     <div class="absolute top-[5%] -left-[20%] w-[50%] h-[100%] rounded-full"
-        style="background: radial-gradient(circle, rgba(230, 120, 120, 0.8) 0%, rgba(242, 219, 219, 0.4) 40%, rgba(255, 255, 255, 0) 50%);">
+    style="background: radial-gradient(circle, rgba(126, 194, 240, 0.8) 0%, rgba(222, 242, 255, 0.4) 40%, rgba(255, 255, 255, 0) 50%);">
     </div>
 
     <!-- Top-right Circular Gradient (Bluish) - Exactly 50% visible -->
     <div class="absolute top-[5%] -right-[20%] w-[50%] h-[100%] rounded-full"
-        style="background: radial-gradient(circle, rgba(126, 194, 240, 0.8) 0%, rgba(222, 242, 255, 0.4) 40%, rgba(255, 255, 255, 0) 50%);">
+    style="background: radial-gradient(circle, rgba(230, 120, 120, 0.8) 0%, rgba(242, 219, 219, 0.4) 40%, rgba(255, 255, 255, 0) 50%);">
+
     </div>
 
 
@@ -44,7 +45,7 @@
                 data-aos="fade-up" data-aos-duration="800" data-aos-mirror="true">
                 <span class="text-secondary-500">ONDC Credit</span>
             </h1>
-            <p class="text-base sm:text-lg  text-gray-700 max-w-3xl mx-auto" data-aos="fade-up"
+            <p class="text-base text-lg  text-gray-700 max-w-3xl mx-auto" data-aos="fade-up"
                 data-aos-duration="800" data-aos-delay="100" data-aos-mirror="true">
                 Simplify your operations with a comprehensive platform offering </br>essential financial, travel, and utility services tailored to your</br> business needs.  
             </p>
